7844ad589641f27f04b592a88f888825aeedf031
Maanas Nair <63333626+MaanasNair@users.noreply.github.com>
2020-12-07 19:24:15 +0530

    Usless CPPPPPPPPPPPPPPPPPPPPPPPPPPP 2

--- app.cpp
+++ app.cpp
@@ -8,3 +8,43 @@ int main() {
     greet();
     ... .. ...
 }
+#include<iostream>
+//calling a function
+greet() {
+    //code
+}
+int main() {
+    ... .. ...
+    greet();
+    ... .. ...
+}
+#include<iostream>
+//calling a function
+greet() {
+    //code
+}
+int main() {
+    ... .. ...
+    greet();
+    ... .. ...
+}
+#include<iostream>
+//calling a function
+greet() {
+    //code
+}
+int main() {
+    ... .. ...
+    greet();
+    ... .. ...
+}
+#include<iostream>
+//calling a function
+greet() {
+    //code
+}
+int main() {
+    ... .. ...
+    greet();
+    ... .. ...
+}
